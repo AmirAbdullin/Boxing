3d7e5b67172369ff19867d74fa6ce2f61acbeb59
Али Салимов <lalalqal@gmail.com>
2022-07-13 06:02:55 +0300

    Добавьте файлы проекта.

---
+++ Boxing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Data\\0\\100_cl.json");
+            var json = File.ReadAllText(path);
+
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject <Models.Input_JSON>(json);
+
+            Console.WriteLine(data.Cargo_groups[0].Id);
+        }
+    }
+}
